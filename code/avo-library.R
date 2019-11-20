d19d7db5368b729852af7cbc62b36e4a41d9acef
cord phelps <rc.phelps@gmail.com>
2019-11-19 19:44:00 -0800

    dash

--- code/avo-library.R
+++ code/avo-library.R
@@ -26,5 +26,33 @@ fmt_dcimals <- function(decimals=0){
 }
 
 
+plotNitrogen <- function(df, saveFile, savePath) {
 
+  nitrogen <- ggplot(data = df, aes(x=year)) + 
+  
+  geom_boxplot(aes(y = nitrateNitrogen.lbsAF, group=year, color=year), shape=21) +
+  #geom_dotplot(aes(y = nitrateNitrogen.lbsAF, group=year, color=year), binaxis='y', stackdir='center', dotsize=1) +
+  #geom_dotplot(aes(y = nitrateNitrogen.lbsAF), binaxis='y', stackdir='center', dotsize=1)
+  
+  scale_y_continuous(trans = 'log10') +
+
+  theme_bw() +
+  theme(axis.title.y.right = element_text(angle = 90)) 
+
+  if (saveFile) {
+
+    fileName <- paste("ggsave.nitrogen.pdf", sep="")
+  
+    if (file.exists(fileName)) { file.remove(fileName) }
+      
+    ggsave(fileName, 
+            plot = nitrogen, 
+            device = NULL, path = savePath,
+            scale = 1, width = 6, height = NA, dpi = 300, limitsize = TRUE,
+            units = c("in", "cm", "mm"))
+  
+  }
+
+  return(nitrogen)
+}
 
